3f8759e0038d5ce09ea2229c63b57049c91db50b
Elian <d2099080@gmail.com>
2025-03-25 14:56:13 +0800

    Set default parameters of simulation
    According to the paper [1], set the default simulation parameters
    of Hybrid Lifi/Wifi network.
    
    [1] H. Liu et al., "Multi-Cell Resource Allocation Mechanism Based
    on Interference Control in Indoor Multicolor VLC-WiFi Heterogeneous
    Networks," in IEEE Transactions on Network and Service Management,
    vol. 21, no. 5, pp. 5707-5717, Oct. 2024, doi:
    10.1109/TNSM.2024.3432148.
    
    "

---
+++ main.py
@@ -0,0 +1,41 @@
+# Room size
+L = 10 # m
+W = 10 # m
+H = 3 # m
+# Height of receiving plan
+H_pd = 1.2 # m
+# Transmit optical power per LED
+P_vlc = 20 # W
+# VLC system bandwidth
+B_vlc = 20 # MHz
+# Semi-angle at half power
+semi_angle_at_helf_power = 60 # degree 
+# Optical filter gain
+optical_filter_gain = 1 
+# Physical area of the PD
+A_pd = 1 # cm^2
+# O/E conversion coefficient
+R_oe = 0.44 # A/W
+G_oe = 0.23 # A/W
+B_oe = 0.15 # A/W
+# Wifi transmit power
+P_wifi = 20 # dBm 
+# Wifi bandwidth
+B_wifi = 20 # MHz
+# Wifi noise spectral density
+N_wifi = -174 # dBm/Hz
+# Wifi receiver power range
+P_w_min = -125 # dBm
+P_w_max = 50 # dBm
+# PD light power range
+P_v_min = 50 # uW
+P_v_max = 10 # mW
+
+# Number of VLC APs
+N_vlc = 16
+# Number of WiFi APs
+N_wifi = 1
+# Default number of UEs
+N_ue = 25
+# Default field of view angle of PD
+FoV = 60 # degree
